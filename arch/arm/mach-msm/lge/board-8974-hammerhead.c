bafb85dc8fc992cf2dbdc2ccd7049220d51bcac1
Fenix46 <scarlataemanuele@gmail.com>
2021-05-18 12:24:45 +0200

    ARM: hammerhead: add limit_mem and rework for 3.10
    add limit_mem cherry-picked from it https://github.com/Fenix46/Kernel-hammerhead/commit/d5cdb0a1b79355cf344c78783f50d3367ecf4146#diff-d653a95a4836128f0eb1ae2be62169e8d9a097ebd031031183d319503de1b028
    and some headers to match with 3.10
    
    Signed-off-by: Fenix46 <scarlataemanuele@gmail.com>

--- arch/arm/mach-msm/lge/board-8974-hammerhead.c
+++ arch/arm/mach-msm/lge/board-8974-hammerhead.c
@@ -14,23 +14,28 @@
 #include <linux/kernel.h>
 #include <linux/platform_device.h>
 #include <linux/io.h>
+#include <linux/i2c/i2c-qup.h>
 #include <linux/irq.h>
 #include <linux/irqdomain.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/of_platform.h>
 #include <linux/memory.h>
+#include <linux/memblock.h>
 #include <linux/regulator/machine.h>
+#include <linux/regulator/qpnp-regulator.h>
 #include <linux/regulator/krait-regulator.h>
 #include <linux/regulator/rpm-smd-regulator.h>
+#include <linux/regulator/spm-regulator.h>
+#include <linux/clk/msm-clk-provider.h>
 #include <asm/mach/map.h>
 #include <asm/mach/map.h>
 #include <asm/mach/arch.h>
 #include <mach/board.h>
 #include <mach/gpiomux.h>
+#include <linux/msm-bus.h>
 #include <mach/msm_iomap.h>
 #include <mach/msm_memtypes.h>
-#include <soc/qcom/restart.h>
 #include <soc/qcom/rpm-smd.h>
 #include <soc/qcom/socinfo.h>
 #include <soc/qcom/smd.h>
@@ -42,12 +47,59 @@
 #include "platsmp.h"
 #include <mach/board_lge.h>
 
+static unsigned long limit_mem;
+
+static int __init limit_mem_setup(char *param)
+{
+	limit_mem = memparse(param, NULL);
+	return 0;
+}
+early_param("limit_mem", limit_mem_setup);
+
+static void __init limit_mem_reserve(void)
+{
+	unsigned long to_remove;
+	unsigned long reserved_mem;
+	unsigned long i;
+	phys_addr_t base;
+
+	if (!limit_mem)
+		return;
+
+	reserved_mem = ALIGN(memblock.reserved.total_size, PAGE_SIZE);
+
+	to_remove = memblock.memory.total_size - reserved_mem - limit_mem;
+
+	pr_info("Limiting memory from %lu KB to to %lu kB by removing %lu kB\n",
+			(memblock.memory.total_size - reserved_mem) / 1024,
+			limit_mem / 1024,
+			to_remove / 1024);
+
+	/* First find as many highmem pages as possible */
+	for (i = 0; i < to_remove; i += PAGE_SIZE) {
+		base = memblock_find_in_range(memblock.current_limit,
+				MEMBLOCK_ALLOC_ANYWHERE, PAGE_SIZE, PAGE_SIZE);
+		if (!base)
+			break;
+		memblock_remove(base, PAGE_SIZE);
+	}
+	/* Then find as many lowmem 1M sections as possible */
+	for (; i < to_remove; i += SECTION_SIZE) {
+		base = memblock_find_in_range(0, MEMBLOCK_ALLOC_ACCESSIBLE,
+				SECTION_SIZE, SECTION_SIZE);
+		if (!base)
+			break;
+		memblock_remove(base, SECTION_SIZE);
+	}
+}
+
 void __init msm_8974_reserve(void)
 {
 	of_scan_flat_dt(dt_scan_for_memory_reserve, NULL);
 #ifdef CONFIG_PSTORE_RAM	
 	lge_reserve();
-#endif	
+#endif
+ 	limit_mem_reserve();
 }
 
 /*
@@ -61,8 +113,11 @@ void __init msm8974_add_drivers(void)
 	msm_smd_init();
 	msm_rpm_driver_init();
 	msm_pm_sleep_status_init();
+	spm_regulator_init();
 	rpm_smd_regulator_driver_init();
 	msm_spm_device_init();
+	msm_bus_fabric_init_driver();
+	qup_i2c_init_driver();
 	krait_power_init();
 #ifdef CONFIG_PSTORE_RAM	
 	lge_add_persistent_device();
@@ -134,7 +189,7 @@ void __init msm8974_init(void)
 	msm8974_add_drivers();
 }
 
-static const char *msm8974_dt_match[] __initconst = {
+static const char *const msm8974_dt_match[] __initconst = {
 	"qcom,msm8974",
 	"qcom,apq8074",
 	NULL
