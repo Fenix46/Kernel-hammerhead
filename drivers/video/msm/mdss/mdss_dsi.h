36e9e4d0577925b5b3c06c2829df4867d1d1bd9e
Fenix46 <scarlataemanuele@gmail.com>
2021-05-20 15:42:16 +0200

    Revert "msm: mdss: dsi: add pinctrl support"
    This reverts commit e6650504a0da5a9aaf57a013586d281cec188979.

--- drivers/video/msm/mdss/mdss_dsi.h
+++ drivers/video/msm/mdss/mdss_dsi.h
@@ -18,7 +18,6 @@
 #include <linux/mdss_io_util.h>
 #include <mach/scm-io.h>
 #include <linux/irqreturn.h>
-#include <linux/pinctrl/consumer.h>
 
 #include "mdss_panel.h"
 #include "mdss_dsi_cmd.h"
@@ -215,12 +214,6 @@ struct dsi_drv_cm_data {
 	int broadcast_enable;
 };
 
-struct dsi_pinctrl_res {
-	struct pinctrl *pinctrl;
-	struct pinctrl_state *gpio_state_active;
-	struct pinctrl_state *gpio_state_suspend;
-};
-
 enum {
 	DSI_CTRL_0,
 	DSI_CTRL_1,
@@ -316,8 +309,6 @@ struct mdss_dsi_ctrl_pdata {
 
 	struct dsi_buf tx_buf;
 	struct dsi_buf rx_buf;
-
-	struct dsi_pinctrl_res pin_res;
 };
 
 int dsi_panel_device_register(struct device_node *pan_node,
