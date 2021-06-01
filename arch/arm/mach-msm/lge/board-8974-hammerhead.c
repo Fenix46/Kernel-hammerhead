ac1c4e537359ce27525560066a593501166f5c4b
Fenix46 <scarlataemanuele@gmail.com>
2021-06-01 06:26:15 +0200

    ARM: board-hammerhead: fixed headers error
    Signed-off-by: Fenix46 <scarlataemanuele@gmail.com>

--- arch/arm/mach-msm/lge/board-8974-hammerhead.c
+++ arch/arm/mach-msm/lge/board-8974-hammerhead.c
@@ -30,10 +30,9 @@
 #include <mach/gpiomux.h>
 #include <mach/msm_iomap.h>
 #include <mach/msm_memtypes.h>
-#include <soc/qcom/restart.h>
+#include <mach/msm_smd.h>
 #include <soc/qcom/rpm-smd.h>
 #include <soc/qcom/socinfo.h>
-#include <soc/qcom/smd.h>
 #include <soc/qcom/smem.h>
 #include <soc/qcom/spm.h>
 #include <soc/qcom/pm.h>
